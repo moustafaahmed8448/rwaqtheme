5a2091d165a9e494abe0d84f3d804158f1c9e340
Moustafa Ahmed <moustafa.ahmed8448@gmail.com>
2026-06-10 15:10:21 +0300

    push to git

--- functions.php
+++ functions.php
@@ -1,18 +1,18 @@
 <?php
 
-function momorwaqtheme_setup() {
+function rwaqtheme_setup() {
   add_theme_support('title-tag');
   add_theme_support('post-thumbnails');
   add_theme_support('html5', array('search-form', 'comment-form', 'comment-list', 'gallery', 'caption'));
   register_nav_menus(array(
-    'primary' => __('Primary Menu', 'momorwaqtheme'),
+    'primary' => __('Primary Menu', 'rwaqtheme'),
   ));
 }
-add_action('after_setup_theme', 'momorwaqtheme_setup');
+add_action('after_setup_theme', 'rwaqtheme_setup');
 
-function momorwaqtheme_assets() {
-  wp_enqueue_style('momorwaqtheme-style', get_stylesheet_uri(), array(), '1.0.0');
-  wp_enqueue_style('momorwaqtheme-main', get_template_directory_uri() . '/assets/css/main.css', array(), '1.0.0');
-  wp_enqueue_script('momorwaqtheme-main', get_template_directory_uri() . '/assets/js/main.js', array(), '1.0.0', true);
+function rwaqtheme_assets() {
+  wp_enqueue_style('rwaqtheme-style', get_stylesheet_uri(), array(), '1.0.0');
+  wp_enqueue_style('rwaqtheme-main', get_template_directory_uri() . '/assets/css/main.css', array(), '1.0.0');
+  wp_enqueue_script('rwaqtheme-main', get_template_directory_uri() . '/assets/js/main.js', array(), '1.0.0', true);
 }
-add_action('wp_enqueue_scripts', 'momorwaqtheme_assets');
+add_action('wp_enqueue_scripts', 'rwaqtheme_assets');
